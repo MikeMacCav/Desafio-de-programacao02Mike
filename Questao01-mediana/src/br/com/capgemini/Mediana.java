a9dd293a54abc185289df35be2b6a862b8f7cc3d
MikeMacCav <michaelmcavalcante@gmail.com>
2022-03-03 23:08:11 -0300

    Questao 01 mediana criada

---
+++ Questao01-mediana/src/br/com/capgemini/Mediana.java
@@ -0,0 +1,20 @@
+package br.com.capgemini;
+import java.util.*;
+
+class Mediana {
+
+ public static double findmediana(int vetor[], int n) // função de cálculo da mediana
+ {  
+     	Arrays.sort(vetor);     
+     			if (n % 2 != 0)
+     				return (double)vetor[n/2];
+     					return (double)(vetor[(n-1)/2] + vetor[n/2]) / 2.0;
+ }
+ 
+ public static void main(String args[])
+ {
+     int vetor[] = {9, 2, 1, 4, 6};
+     int n = vetor.length;
+     System.out.println("mediana = " + findmediana(vetor, n));
+ }
+}
